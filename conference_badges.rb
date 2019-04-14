a59e84082b4763048a029aa9ad5db402d92aba60
Carla Stickler <carlastickler@gmail.com>
2019-04-14 20:05:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badge_names = []
+  names.each do |name|
+    badge_names.push("Hello, my name is #{name}.")
+  end
+  badge_names
+end
+
+def assign_rooms(names)
+  counter = 1 
+  
