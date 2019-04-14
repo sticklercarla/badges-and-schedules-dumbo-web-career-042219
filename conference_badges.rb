c1361579763c6cadcb6dcde9a0a9d9286226443a
Carla Stickler <carlastickler@gmail.com>
2019-04-14 20:17:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 
 def assign_rooms(names)
   room_assignments = []
-  names.each_with_index do |name|
+  names.each_with_index do |name, index|
     room_assignments.push("Hello, #{name}! You'll be assigned to room #{i + 1}!")
   end
   room_assignments
